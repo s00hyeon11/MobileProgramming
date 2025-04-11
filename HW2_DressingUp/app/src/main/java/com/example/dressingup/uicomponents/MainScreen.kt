ed65cd606698c740cfba247b2358783572dce74c
wa4abi <heukimjalatte@gmail.com>
2025-04-11 23:58:48 +0900

    fix. LazyGrid 적용

--- HW2_DressingUp/app/src/main/java/com/example/dressingup/uicomponents/MainScreen.kt
+++ HW2_DressingUp/app/src/main/java/com/example/dressingup/uicomponents/MainScreen.kt
@@ -10,6 +10,12 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyHorizontalGrid
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Checkbox
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -24,14 +30,95 @@ import com.example.dressingup.model.ClothingItem
 import com.example.dressingup.viewmodel.ClothingViewModel
 
 @Composable
-fun MainScreen() {
+fun MainScreen(
+    modifier: Modifier = Modifier,
+    clothingViewModel: ClothingViewModel = viewModel()
+) {
     val orientation = LocalConfiguration.current.orientation
-
+    val visibility = clothingViewModel.visibility
+    val itemList = ClothingItem.itemList
 
     if (orientation == Configuration.ORIENTATION_PORTRAIT) {
-        PortraitLayout()
+        Column(
+            modifier = Modifier.padding(vertical = 16.dp)
+        ) {
+            Box {
+                Image(
+                    painter = painterResource(R.drawable.body),
+                    contentDescription = "body",
+                    modifier = Modifier
+                )
+
+                itemList.forEach() {
+                    if (visibility[it] == true) {
+                        val imageRes = it.imageRes
+                        Image(
+                            painter = painterResource(imageRes),
+                            contentDescription = it.id,
+                            modifier = Modifier
+                        )
+                    }
+                }
+            }
+            LazyVerticalGrid(
+                columns = GridCells.Fixed(2)
+            ) {
+                items(itemList) {
+                    Row {
+                        Checkbox(
+                            checked = visibility[it] == true,
+                            onCheckedChange = { isChecked ->
+                                clothingViewModel.setVisibility(it, isChecked)
+                            }
+                        )
+                        Text(text = it.id)
+                    }
+                }
+            }
+            Text(text = "202212376 차수현")
+        }
     } else {
-        LandscapeLayout()
+        Row {
+            Box {
+                Image(
+                    painter = painterResource(R.drawable.body),
+                    contentDescription = "body",
+                    modifier = Modifier
+                )
+
+                itemList.forEach() {
+                    if (visibility[it] == true) {
+                        val imageRes = it.imageRes
+                        Image(
+                            painter = painterResource(imageRes),
+                            contentDescription = it.id,
+                            modifier = Modifier
+                        )
+                    }
+                }
+            }
+            Column(
+                modifier = Modifier.padding(vertical = 32.dp)
+            ) {
+                LazyVerticalGrid(
+                    columns = GridCells.Fixed(2)
+                ) {
+                    items(itemList) {
+                        Row {
+                            Checkbox(
+                                checked = visibility[it] == true,
+                                onCheckedChange = { isChecked ->
+                                    clothingViewModel.setVisibility(it, isChecked)
+                                }
+                            )
+                            Text(text = it.id)
+                        }
+                    }
+                }
+                Text(text = "202212376 차수현")
+            }
+
+        }
     }
 }
 
